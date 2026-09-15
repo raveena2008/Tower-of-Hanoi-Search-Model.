06b952bfe55d3da65ccb2aa65ffb3984d923e2b9
raveena2008 <raveenabose.c@gmail.com>
2026-09-15 15:11:44 +0530

    Refactor Tower of Hanoi to use functions

--- Src/main.py
+++ Src/main.py
@@ -1,96 +1,103 @@
 from collections import deque
 
 
-class TowerOfHanoiSearch:
-    def __init__(self, disks):
-        self.disks = disks
-        self.start = tuple(
-            [tuple(range(disks, 0, -1)), (), ()]
-        )
-        self.goal = ((), (), tuple(range(disks, 0, -1)))
+def get_valid_moves(state):
+    """
+    Generate all valid moves from the current state.
+    """
 
-    def get_moves(self, state):
-        moves = []
+    moves = []
 
-        for source in range(3):
-            if not state[source]:
+    for source in range(3):
+        if not state[source]:
+            continue
+
+        for target in range(3):
+            if source == target:
                 continue
 
-            disk = state[source][-1]
+            if not state[target] or state[source][-1] < state[target][-1]:
 
-            for target in range(3):
-                if source == target:
-                    continue
+                new_state = [list(peg) for peg in state]
 
-                if not state[target] or state[target][-1] > disk:
-                    new_state = [list(rod) for rod in state]
+                disk = new_state[source].pop()
+                new_state[target].append(disk)
 
-                    new_state[target].append(
-                        new_state[source].pop()
+                moves.append(
+                    (
+                        tuple(tuple(peg) for peg in new_state),
+                        (source, target)
                     )
+                )
 
-                    moves.append(
-                        (tuple(tuple(rod) for rod in new_state),
-                         source, target)
+    return moves
+
+
+def solve_hanoi_bfs(number_of_disks):
+    """
+    Solve Tower of Hanoi using Breadth-First Search.
+    """
+
+    initial_state = (
+        tuple(range(number_of_disks, 0, -1)),
+        (),
+        ()
+    )
+
+    goal_state = (
+        (),
+        (),
+        tuple(range(number_of_disks, 0, -1))
+    )
+
+    queue = deque([(initial_state, [])])
+    visited = {initial_state}
+
+    while queue:
+
+        current_state, path = queue.popleft()
+
+        if current_state == goal_state:
+            return path
+
+        for next_state, move in get_valid_moves(current_state):
+
+            if next_state not in visited:
+
+                visited.add(next_state)
+
+                queue.append(
+                    (
+                        next_state,
+                        path + [move]
                     )
+                )
 
-        return moves
+    return None
 
-    def bfs(self):
-        queue = deque([(self.start, [])])
-        visited = {self.start}
 
-        while queue:
-            state, path = queue.popleft()
+def display_solution(number_of_disks):
 
-            if state == self.goal:
-                return path
+    solution = solve_hanoi_bfs(number_of_disks)
 
-            for new_state, source, target in self.get_moves(state):
-                if new_state not in visited:
-                    visited.add(new_state)
+    if solution is None:
+        print("No solution found.")
+        return
 
-                    new_path = path + [
-                        (source + 1, target + 1)
-                    ]
+    print(f"Number of disks: {number_of_disks}")
+    print(f"Minimum moves: {len(solution)}")
+    print("\nMove sequence:")
 
-                    queue.append((new_state, new_path))
+    for step, (source, target) in enumerate(solution, start=1):
 
-        return None
-
-    def display_solution(self):
-        solution = self.bfs()
-
-        if solution is None:
-            print("No solution found.")
-            return
-
-        print("\nTower of Hanoi Search Model")
-        print("---------------------------")
-        print("Number of disks:", self.disks)
-        print("Algorithm: Breadth-First Search")
-        print("Total moves:", len(solution))
-        print("\nSolution steps:")
-
-        for step, move in enumerate(solution, 1):
-            source, target = move
-            print(
-                f"Step {step}: Move disk from "
-                f"Tower {source} to Tower {target}"
-            )
+        print(
+            f"Step {step}: Move disk from "
+            f"Peg {source + 1} to Peg {target + 1}"
+        )
 
 
 if __name__ == "__main__":
-    try:
-        disks = int(input("Enter number of disks: "))
 
-        if disks <= 0:
-            print("Please enter a positive number.")
-        elif disks > 8:
-            print("Please enter 8 or fewer disks.")
-        else:
-            model = TowerOfHanoiSearch(disks)
-            model.display_solution()
+    disks = int(input("Enter number of disks: "))
 
-    except ValueError:
-        print("Please enter a valid number.")
+    display_solution(disks)
